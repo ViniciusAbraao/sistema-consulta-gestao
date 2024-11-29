1327f411c7b096f70be5834d483dbd17e30deee3
Chrissdy12 <chris19sdy@gmail.com>
2024-11-29 00:35:57 -0300

    Adicionado funções

--- src/main/java/com/sistemaconsulta/gestao/model/domain/Medico.java
+++ src/main/java/com/sistemaconsulta/gestao/model/domain/Medico.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalTime;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +12,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,6 +32,13 @@ public class Medico implements Serializable {
 
 	private String nome;
 
+	
+    @NotBlank
+    @Column(nullable = false)
+	@Size(min = 6, max = 6)
+    private String crm;
+
+   
 	@ManyToOne
 	@JoinColumn(name = "especialidade_id")
 	private Especialidade especialidade;
